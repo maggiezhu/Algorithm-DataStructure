6396b399386d53b359c5487fe6829c1006214e5d
Tong Zhu <tongzhu@Tongs-MacBook-Pro-2.local>
2018-12-10 21:32:11 -0500

    BigO notes

--- BigO.js
+++ BigO.js
@@ -1,10 +1,20 @@
 /*
+    - Time Complexity 
+      BigO      
+      Object: 
+        insert, removal, change = O(1)
+        search for a value = O(n)
+        .keys(),.values(),.entries() = O(n)
+        .hasOwnProperty() = O(1) -> pass in a key and see if it exist
+
+
+    - Space Complexity(auxiliary space complexity)
+      recursion concerins with space complexity 
+      most preimitive are constant space (booleans, number, undefined)
+      String: O(n) space 
+      Object: O(n) where n is the number of keys
+
+    
 
 
 */
-let n = process.argv[2];
-
-function addUpTo(n) {
-  console.log(n * (n + 1) / 2);
-}
-addUpTo(n);
